84c7ea952279e7a794c7c5daae027189bce48144
Guillaume Lopez <guillaumelopezgk@hotmail.com>
2023-03-06 09:10:37 +0900

    app setup

---
+++ agency/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+    <BrowserRouter>
+        <Routes>
+            <Route />
+        </Routes>
+    
+    </BrowserRouter>
+};
+
+export default App;
